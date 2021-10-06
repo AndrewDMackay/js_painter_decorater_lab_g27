86644b5bd9939c02040bfc774003df6d06a2d60c
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-06 09:28:15 +0100

    6th commit main

--- models/decorator.js
+++ models/decorator.js
@@ -2,7 +2,7 @@ const Room = require("./room");
 
 const Decorator = function(){
     this.stock = [];
-    this.emptyPaint = [];
+    this.emptyStock = [];
 };
 
 
@@ -64,33 +64,21 @@ Decorator.prototype.removePaintByName = function(paint){
 }
 
 
-Decorator.prototype.removeAllEmptyPaint = function(){
-    this.emptyPaint.splice(0, this.emptyPaint.length);
-}
-
-
-Decorator.prototype.sortEmptyPaint = function(){
+Decorator.prototype.sortEmptyStock = function(){
     for (paint of this.stock){
         if (paint.empty === true){
-            this.emptyPaint.push(paint);
+            this.emptyStock.push(paint);
     } else {
         return 'There are no empty paint cans..'
     }
 }}
 
 
+
+Decorator.prototype.removeAllEmptyStock = function(){
+    this.emptyStock.splice(0, this.emptyStock.length);
+}
+
+
 module.exports = Decorator;
 
-
-// Extensions
-
-// A decorator should..
-
-// should be able to remove empty paint cans from stock..
-
-// Hint..
-
-// We want to avoid removing items from arrays while iterating over them.. 
-// This is because iteration uses the index number to access the current item.. 
-// And, if you remove an item during one of the iterations, the position of each of the following items will be changed.. 
-// And, lead to unexpected results..
